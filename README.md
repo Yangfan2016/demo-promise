9377c75091da07d709bf5a681c7ac8d448487353
yangfan2016 <15234408101@163.com>
2019-01-02 15:36:52 +0800

    add: init finish

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # demo-promise
 This is a promise demo base Promises/A+
+
+
+## docs
+https://malcolmyu.github.io/2015/06/12/Promises-A-Plus/#Promise  
+
+
+## Test
+```bash
+$ npm i -g promises-aplus-tests
+$ promises-aplus-tests Promise.js
+```
--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+// helpers
+function _isFunc(obj) {
+    return typeof obj === "function";
+}
+
 class YPromise {
     constructor(handler) {
         this.status = "pending";
         this.value;
         this.reason;
+        this.succHandlerList = []; // for async
+        this.errHandlerList = []; // for async
 
         let resolve = (value) => {
             if (this.status === "pending") {
                 this.status = "resolved";
                 this.value = value;
+                this.succHandlerList.forEach(fn => {
+                    fn(value);
+                });
             }
         }
 
@@ -15,6 +25,9 @@ class YPromise {
             if (this.status === "pending") {
                 this.status = "rejected";
                 this.reason = err;
+                this.errHandlerList.forEach(fn => {
+                    fn(err);
+                });
             }
         }
 
@@ -25,10 +38,110 @@ class YPromise {
         }
     }
     then(callback, fallback) {
-        if (this.status === "resolved") {
-            callback(this.value);
-        } else {
-            fallback(this.reason);
+        callback = _isFunc(callback) ? callback : value => value;
+        fallback = _isFunc(fallback) ? fallback : err => { throw err; }
+        let promise;
+        switch (this.status) {
+            case 'pending': // for async
+                promise = new YPromise((resolve, reject) => {
+                    this.succHandlerList.push(() => {
+                        // to be async https://promisesaplus.com/#point-67
+                        setTimeout(() => {
+                            try {
+                                let returnVal = callback(this.value);
+                                YPromise.resolvePromise(promise, returnVal, resolve, reject);
+                            } catch (err) {
+                                reject(err);
+                            }
+                        }, 0);
+                    });
+                    this.errHandlerList.push(() => {
+                        setTimeout(() => {
+                            try {
+                                let returnVal = fallback(this.reason);
+                                YPromise.resolvePromise(promise, returnVal, resolve, reject);
+                            } catch (err) {
+                                reject(err);
+                            }
+                        }, 0);
+                    });
+                });
+                break;
+            case 'resolved':
+                promise = new YPromise((resolve, reject) => {
+                    setTimeout(() => {
+                        try {
+                            let returnVal = callback(this.value);
+                            YPromise.resolvePromise(promise, returnVal, resolve, reject);
+                        } catch (err) {
+                            reject(err);
+                        }
+                    }, 0);
+                });
+                break;
+            case 'rejected':
+                promise = new YPromise((resolve, reject) => {
+                    setTimeout(() => {
+                        try {
+                            let returnVal = fallback(this.reason);
+                            YPromise.resolvePromise(promise, returnVal, resolve, reject);
+                        } catch (err) {
+                            reject(err);
+                        }
+                    }, 0);
+                });
+                break;
+            default:
+                // nothing
+                break;
+        }
+
+        return promise;
+    }
+    static resolvePromise(promise, x, resolve, reject) {
+        if (x instanceof YPromise) {
+            if (x.status === "pending") {
+                x.then(data => {
+                    YPromise.resolvePromise(promise, data, resolve, reject);
+                }, err => {
+                    reject(err);
+                });
+            } else {
+                x.then(resolve, reject);
+            }
+        } else { // noraml value
+            resolve(x);
         }
     }
+    static resolve(value) {
+        return new YPromise(function (resolve) {
+            resolve(value);
+        });
+    }
+    static reject(err) {
+        return new YPromise(function (resolve, reject) {
+            reject(value);
+        });
+    }
 }
+
+
+// TEST
+new YPromise(function (resolve, reject) {
+    resolve(1);
+}).then(function (data) {
+    console.log(data);
+    return YPromise.resolve(2);
+}).then(function (data) {
+    console.log(data);
+    return new YPromise(function (resolve) {
+        setTimeout(function () {
+            resolve(3)
+        }, 0);
+    });
+}).then(function (data) {
+    console.log(data);
+    return 4;
+}).then(function (data) {
+    console.log(data);
+});
